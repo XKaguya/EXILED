3872aa3277a2c980994e30744fc5a251650d8a6d
辉夜 <rx700@vip.qq.com>
2024-07-21 02:09:18 +0800

    Update PlayingFootstepEventArgs.cs

--- EXILED/Exiled.Events/EventArgs/Scp939/PlayingFootstepEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Scp939/PlayingFootstepEventArgs.cs
@@ -14,37 +14,40 @@ namespace Exiled.Events.EventArgs.Scp939
     /// <summary>
     /// Contains all information before the footsteps are being shown to SCP-939.
     /// </summary>
-    public class PlayingFootstepEventArgs : IScp939Event, IDeniableEvent
+    public class PlayingFootstepEventArgs : IScp939Event, IDeniableEvent, IPlayerEvent
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="PlayingFootstepEventArgs" /> class.
         /// </summary>
         /// <param name="player">
-        /// The target being shown to SCP-939.
-        /// </param>
-        /// <param name="scp939">
         /// The player who's controlling SCP-939.
+        /// </param>
+        /// <param name="target">
+        /// The player who's being shown to SCP-939.
         /// </param>
         /// <param name="isAllowed">
         /// Indicates whether the footstep action is allowed.
         /// </param>
-        public PlayingFootstepEventArgs(Player player, Player scp939, bool isAllowed = true)
+        public PlayingFootstepEventArgs(Player target, Player player, bool isAllowed = true)
         {
             Player = player;
-            Scp939 = scp939.Role.As<Scp939Role>();
+            Scp939 = Player.Role.As<Scp939Role>();
+            Target = target;
             IsAllowed = isAllowed;
         }
 
         /// <summary>
-        /// Gets the target being shown to SCP-939.
+        /// Gets the player who's controlling SCP-939.
         /// </summary>
         public Player Player { get; }
 
         /// <inheritdoc/>
-        /// <summary>
-        /// Gets the player who's controlling SCP-939.
-        /// </summary>
         public Scp939Role Scp939 { get; }
+
+        /// <summary>
+        /// Gets the player who's being shown to SCP-939.
+        /// </summary>
+        public Player Target { get; }
 
         /// <summary>
         /// Gets or sets a value indicating whether footsteps are visible.
